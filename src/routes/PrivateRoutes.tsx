187f42a2c78e5bc8be2bcace281d9af69159e0a0
Filip Dordevic <filipdordevic.srb@gmail.com>
2023-09-22 20:28:59 +0200

    feat: add landing page for users when not signed in

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -1,11 +1,12 @@
 import { FunctionComponent } from "react";
 import { Outlet } from "react-router-dom";
 import ExternalNavigate from "./ExternalNavigate";
+import { useUserAccount } from "~/services/settings/userAccount";
 
 interface PrivateRoutesProps {}
 
 const PrivateRoutes: FunctionComponent<PrivateRoutesProps> = () => {
-  const user = true;
+  const { data: user } = useUserAccount();
   if (!user)
     return <ExternalNavigate to={import.meta.env.VITE_DJFAN_SIGN_IN_URL} />;
   else return <Outlet />;
