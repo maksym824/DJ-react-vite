c7bcfdf42c6d4cf30e12c7e9a2441b0560fae334
Filip Dordevic <filipdordevic.srb@gmail.com>
2023-11-29 18:15:05 +0100

    feat: [DJFAN-444]: add DM channels and channel list in DJ chat

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,9 +1,10 @@
 import { Flex } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import { AscDesc } from "stream-chat";
+import { AscDesc, ChannelFilters, Channel as IChannel } from "stream-chat";
 import {
   Channel,
   ChannelHeader,
+  ChannelList,
   LoadingIndicator,
   MessageInput,
   MessageList,
@@ -11,27 +12,61 @@ import {
   useChatContext,
 } from "stream-chat-react";
 import Header from "~/components/Header";
+import getDJDMChannels from "~/services/getDJDMChannels";
 import { useUserData } from "~/services/settings/userData";
+
+interface ChannelData {
+  channel_id: string;
+  message_count: number;
+}
+
+const sort = [{ last_message_at: -1 as AscDesc }];
 
 const ChatPage = () => {
   const { data: userData } = useUserData();
   const { client } = useChatContext();
-  // eslint-disable-next-line
-  const [channel, setChannel] = useState<any>();
+  const [channel, setChannel] = useState<IChannel>();
   const [loading, setLoading] = useState(true);
+  const [dmChannelFilters, setDMChannelFilters] = useState<ChannelFilters>();
 
   const initChannel = async () => {
     const filter = {
       type: "messaging",
       id: { $in: [userData?.user_id?.toString() ?? ""] },
-    };
-    const sort = [{ last_message_at: -1 as AscDesc }];
+    } as ChannelFilters;
     try {
       const channels = await client.queryChannels(filter, sort, {
         watch: true,
         state: true,
       });
       setChannel(channels[0]);
+
+      const dmChannelList: ChannelData[] = await getDJDMChannels();
+
+      const dmChannelFilter = {
+        type: "messaging",
+        $or: [
+          {
+            id: {
+              $in: dmChannelList.map((x) => x.channel_id),
+            },
+          },
+          {
+            id: {
+              $in: [userData?.user_id?.toString() ?? ""],
+            },
+          },
+        ],
+        members: {
+          $in: [userData?.user_id?.toString() ?? ""],
+        },
+      } as ChannelFilters;
+      setDMChannelFilters(dmChannelFilter);
+      // const _channels = await client.queryChannels(dmChannelFilter, sort, {
+      //   watch: true,
+      //   state: true,
+      // });
+      // console.log("_channels", _channels);
     } catch (err) {
       console.log("err", err);
     } finally {
@@ -54,13 +89,16 @@ const ChatPage = () => {
   return (
     <Flex direction="column">
       <Header />
-      <Channel channel={channel}>
-        <Window>
-          <ChannelHeader />
-          <MessageList />
-          <MessageInput grow={true} />
-        </Window>
-      </Channel>
+      <Flex>
+        <ChannelList sort={sort} filters={dmChannelFilters} />
+        <Channel channel={channel}>
+          <Window>
+            <ChannelHeader />
+            <MessageList />
+            <MessageInput grow={true} />
+          </Window>
+        </Channel>
+      </Flex>
     </Flex>
   );
 };
